a75a47dfd47d620e5c469b5aac5dd91442f4016d
itstoasti <128280994+itstoasti@users.noreply.github.com>
2024-10-23 22:44:43 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://social-gathering.onrender.com',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path
