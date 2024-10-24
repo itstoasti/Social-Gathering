2eafb75bc4fdef7a593014be45f716d8a7ccbfad
itstoasti <128280994+itstoasti@users.noreply.github.com>
2024-10-23 19:09:09 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,19 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  },
   server: {
     port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-        secure: false
+        secure: false,
+        rewrite: (path) => path
       }
     }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   }
 });
